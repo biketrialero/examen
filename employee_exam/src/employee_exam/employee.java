4d60bd9a28af42a148cfbf23474dd3f91910706d
biketrialero <cpaparuzsmx2010@gmail.com>
2014-11-18 16:36:07 +0100

    primera versio

---
+++ employee_exam/src/employee_exam/employee.java
@@ -0,0 +1,34 @@
+package employee_exam;
+
+public class employee {
+
+	   private String myName;
+	   private double mySalary;
+
+	   public employee(String name, double salary) {
+	      myName = name;
+	      mySalary = salary;
+	   }
+
+	   public String getName() {
+	      return myName;
+	   }
+
+	   public double getSalary() {
+	      return mySalary;
+	   }
+
+	   public void raiseSalary(int percent) {
+	      mySalary = mySalary +
+	         percent * 0.01 * mySalary;
+	   }
+
+	   public void setSalary(double salary) {
+	      mySalary = salary;
+	   }
+
+	   public String toString() {
+	      return "Name: " + getName() +
+	             ", Salary: " + getSalary();
+	   }
+	}
